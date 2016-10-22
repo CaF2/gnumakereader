6808f4ec759330537a440af54b80882b475a003c
caf2 <florian.evaldsson@telia.com>
2016-10-23 00:41:54 +0200

    removed unnecessary files

--- inc/gen/gmr_misc.h
+++
@@ -1,9 +0,0 @@
-//This file was generated! Dont touch!
-#pragma once
-
-#ifndef inc_gen_gmr_misc_h
-#define inc_gen_gmr_misc_h
-
-char *gmr_get_include_strings(const char *command);
-
-#endif
--- inc/gen/shell.h
+++
@@ -1,9 +0,0 @@
-//This file was generated! Dont touch!
-#pragma once
-
-#ifndef inc_gen_shell_h
-#define inc_gen_shell_h
-
-FILE *shell_execute_from_buffer(const char *str);
-
-#endif
